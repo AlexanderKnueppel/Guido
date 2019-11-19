98214ca5e38bd93d1f93e82d04be89d67e6cd530
Markus Gamperle <M.Gamperle@gmail.com>
2019-11-19 09:55:17 +0100

    changed several Packages

--- Plugins/de.tubs.isf.guido.Key_VerificationSimulator/src/de/tu/bs/guido/key/simulator/options/strategy/StrategyOptionable.java
+++
@@ -1,7 +0,0 @@
-package de.tu.bs.guido.key.simulator.options.strategy;
-
-import de.tu.bs.guido.key.simulator.options.Optionable;
-
-public interface StrategyOptionable extends Optionable{
-	
-}
--- Plugins/de.tubs.isf.guido.Key_VerificationSimulator/src/de/tu/bs/guido/key/simulator/options/taclets/TacletOptionable.java
+++
@@ -1,7 +0,0 @@
-package de.tu.bs.guido.key.simulator.options.taclets;
-
-import de.tu.bs.guido.key.simulator.options.Optionable;
-
-public interface TacletOptionable extends Optionable{
-	
-}
